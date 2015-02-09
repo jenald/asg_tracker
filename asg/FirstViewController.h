3ab10b51aa2f4f26f90b137504732465cd391300
Rey Jenald Peña <rjpena@alistair-ubuntu.gfsecure.localdomain>
2015-02-09 11:07:45 +0800

    Initial Commit

---
+++ asg/FirstViewController.h
@@ -0,0 +1,15 @@
+//
+//  FirstViewController.h
+//  asg
+//
+//  Created by Rey Jenald Peña on 2/9/15.
+//  Copyright (c) 2015 appfibre. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+
+@end
+
---
+++ asgTests/asgTests.m
@@ -0,0 +1,40 @@
+//
+//  asgTests.m
+//  asgTests
+//
+//  Created by Rey Jenald Peña on 2/9/15.
+//  Copyright (c) 2015 appfibre. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface asgTests : XCTestCase
+
+@end
+
+@implementation asgTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
