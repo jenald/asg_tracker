e6335112bd5fba94d78228b82394a2a338d431ff
Rey Jenald Peña <rjpena@wifi.local>
2015-02-27 18:07:22 +0800

    worksite view controller initial code

--- asg/Worksite/ASWorksiteViewController.m
+++ asg/Worksite/ASWorksiteViewController.m
@@ -8,15 +8,20 @@
 
 #import "ASWorksiteViewController.h"
 
-@interface ASWorksiteViewController ()
+@interface ASWorksiteViewController () <UITableViewDataSource, UITableViewDelegate>
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
+@property (weak, nonatomic) IBOutlet UISegmentedControl *segmentControl;
 
 @end
 
-@implementation ASWorksiteViewController
+@implementation ASWorksiteViewController {
+    NSArray *worksites;
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    
 }
 
 - (void)didReceiveMemoryWarning {
@@ -24,14 +29,56 @@
     // Dispose of any resources that can be recreated.
 }
 
-/*
-#pragma mark - Navigation
+#pragma mark - Setup
 
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+- (void)setupNavigationController {
+    
 }
-*/
+
+#pragma mark - Private Methods
+
+#pragma mark - Actions
+
+- (IBAction)segmentControlValueDidChange:(id)sender {
+    switch (self.segmentControl.selectedSegmentIndex) {
+        case 0:{
+            
+        }
+            break;
+        case 1: {
+            
+        }
+            break;
+        default:
+            break;
+    }
+}
+
+
+#pragma mark - UITableViewDataSource
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return 5;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    static NSString *cellIdentifier = @"WorksiteCell";
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellIdentifier forIndexPath:indexPath];
+    if (!cell) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:cellIdentifier];
+    }
+    
+    cell.textLabel.text = [NSString stringWithFormat:@"Worksite %d",indexPath.row];
+    
+    return cell;
+}
+
+#pragma mark - UITableViewDelegate
+
 
 @end
